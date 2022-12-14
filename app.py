e052a10d08263c98f30932f5164ca8b788751ef9
sp0t <Chenyin1g992@gmail.com>
2022-12-13 21:42:33 -0800

    stmart complete

--- app.py
+++ app.py
@@ -31,8 +31,10 @@ user = {"username": "abc", "password": "xyz"}
 # Routes
 @app.route('/', methods = ["GET", "POST"])
 def index(): 
+    print("123")
     if('user' in session and session['user'] != user['username']):
         return '<h1>You are not logged in.</h1>'
+    print("345")
     
     try: 
         del model_1a
@@ -55,33 +57,19 @@ def index():
     except:
         pass
 
+    print("456")
 
     today_schedule = schedule.get_schedule()
     engine = database.connect_to_db()
     last_update = pd.read_sql("SELECT * FROM updates", con = engine).iloc[-1]
+    print("1trt")
+
     last_date, last_time, last_record = last_update["update_date"], last_update["update_time"], last_update["last_record"]
     
+    print("567")
+
     return render_template("index.html", schedule = today_schedule, last_record = last_record, 
                            update_date = last_date, update_time = last_time)
-
-@app.route('/login', methods = ['POST', 'GET'])
-def login():
-    if(request.method == 'POST'):
-        username = request.form.get('username')
-        password = request.form.get('password')     
-        if username == user['username'] and password == user['password']:
-            
-            session['user'] = username
-            return redirect('/home')
-
-        return "<h1>Wrong username or password</h1>"    #if the username or password does not matches 
-
-    return render_template("login.html")
-
-@app.route('/logout')
-def logout():
-    session.pop('user')         #session.pop('user') help to remove the session from the browser
-    return redirect('/login')
 
 @app.route('/get_game_info', methods = ["POST"])
 def get_game_info(): 
@@ -190,11 +178,12 @@ def show_database():
 @app.route('/showbetting', methods = ["GET", "POST"])
 def show_betting():
     engine = database.connect_to_db()
-
+    print("123")
     if request.method == 'GET':
         today = date.today()
         global daystr
         daystr = today.strftime("%Y-%m-%d")
+        print("1234")
 
     if request.method == 'POST':
         modify_data = request.get_json()
@@ -255,7 +244,6 @@ def season_state():
 @app.route('/betting', methods = ["POST"])    
 def betting_proc(): 
     if request.method == 'POST':
-        print("######################create table####################")
         betting_data = request.get_json()
         engine = database.connect_to_db()
         for betting in betting_data:
@@ -267,13 +255,9 @@ def betting_proc():
                                 '\'' + '0' + '\'' +  ',' + '\'' + betting["site"] + '\'' + ');'
     
             engine.execute(betting_table_sql)
-            print("##################insert table####################")
             print(betting_table_sql)
-            print("##################contract data####################")
             betid = engine.execute("SELECT MAX(betid) FROM betting_table;").fetchall()
             if(betting["game"].lower() == "baseball"):
-                print(betid)
-                print(betting)
                 smartContract.createBetData(betid[0][0], betting)
     return
 
--- functions/smartContract.py
+++ functions/smartContract.py
@@ -2,7 +2,7 @@ from web3 import Web3
 
 w3 = Web3(Web3.HTTPProvider('https://rpc-mumbai.maticvigil.com'))
 chainId = 80001
-contractAddress = "0x0DfDcA862dB4730feF06a6f08bABf95547777653"
+contractAddress = "0x129b52Ad452B6e2CA044500b0535443594514402"
 abi = '[{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"betDic","outputs":[{"internalType":"string","name":"betdate","type":"string"},{"internalType":"string","name":"game","type":"string"},{"internalType":"string","name":"teams","type":"string"},{"internalType":"string","name":"market","type":"string"},{"internalType":"string","name":"place","type":"string"},{"internalType":"uint256","name":"stake","type":"uint256"},{"internalType":"uint256","name":"odds","type":"uint256"},{"internalType":"uint256","name":"profitloss","type":"uint256"},{"internalType":"string","name":"status","type":"string"},{"internalType":"string","name":"site","type":"string"},{"internalType":"uint256","name":"createdAt","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_betId","type":"uint256"},{"internalType":"string","name":"_status","type":"string"}],"name":"changeBetstatus","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_betId","type":"uint256"},{"internalType":"string","name":"_betDate","type":"string"},{"internalType":"string","name":"_game","type":"string"},{"internalType":"string","name":"_teams","type":"string"},{"internalType":"string","name":"_market","type":"string"},{"internalType":"string","name":"_place","type":"string"},{"internalType":"uint256","name":"_stake","type":"uint256"},{"internalType":"uint256","name":"_odds","type":"uint256"},{"internalType":"uint256","name":"_profitloss","type":"uint256"},{"internalType":"string","name":"_status","type":"string"},{"internalType":"string","name":"_site","type":"string"}],"name":"createBetData","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]'
 public_key = "0x56a2FD1875eD5B43B3f60DfB1B9087Ca11268509"
 private_key = "7c9fc9d0fe048f58b03d49a277af17e92279c40f6fb172e4820ea508cd144ce9"
@@ -21,24 +21,18 @@ def createBetData(betID, betData):
 
   _market = betData["market"]
   _place = betData["place"]
-  _stake = int(float(betData["stake"]) * 100)
-  _odds = int(float(betData["odds"]) * 100)
-  _profitloss = int(float(betData["wins"]) * 100)
+  _stake = int(float(betData["stake"]) * 1000)
+  _odds = int(float(betData["odds"]) * 1000)
+  _profitloss = int(float(betData["wins"]) * 1000)
   _status = "PENDING"
   _site = betData["site"]
 
-  print("1")
   add_tx = deployed_contract.functions.createBetData( _betId, _betDate, _game, _teams, _market, _place, _stake, _odds, _profitloss, _status, _site)
-  print("2")
   nonce = w3.eth.getTransactionCount(public_key)
-  print("3")
   gasprice = w3.eth.gas_price
-  print("4")
   add_tx = add_tx.buildTransaction({'from': public_key, 'chainId': chainId, 'gasPrice': gasprice, 'nonce': nonce})
-  print("5")
   tx_create = w3.eth.account.sign_transaction(add_tx, private_key)
   txn_hash = w3.eth.sendRawTransaction(tx_create.rawTransaction)
-  print("txn_hash1", txn_hash)
   return
 
 def changeBetStatus(betID, status):
@@ -49,5 +43,4 @@ def changeBetStatus(betID, status):
   add_tx = add_tx.buildTransaction({'from': public_key, 'chainId': chainId, 'gasPrice': gasprice, 'nonce': nonce})
   tx_create = w3.eth.account.sign_transaction(add_tx, private_key)
   txn_hash = w3.eth.sendRawTransaction(tx_create.rawTransaction)
-  print("txn_hash1", txn_hash)
   return

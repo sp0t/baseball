c5aaa9dc460604985a2969a0b70abeedbb2bd3fb
sp0t <Chenyin1g992@gmail.com>
2023-01-13 04:24:23 -0800

    batter-table modify

--- datainsert.py
+++ datainsert.py
@@ -16,49 +16,49 @@ data = df.set_index('game_id').T.to_dict('dict')
 
 
 #create tables
-db.execute("DROP TABLE IF EXISTS game_table;")
+# db.execute("DROP TABLE IF EXISTS game_table;")
 db.execute("DROP TABLE IF EXISTS batter_table;")
-db.execute("DROP TABLE IF EXISTS pitcher_table;")
-db.execute("DROP TABLE IF EXISTS schedule;")
-db.execute("CREATE TABLE IF NOT EXISTS game_table(game_id TEXT, game_date TEXT, away_team TEXT, home_team TEXT, away_score TEXT, home_score TEXT, winner TEXT);")
+# db.execute("DROP TABLE IF EXISTS pitcher_table;")
+# db.execute("DROP TABLE IF EXISTS schedule;")
+# db.execute("CREATE TABLE IF NOT EXISTS game_table(game_id TEXT, game_date TEXT, away_team TEXT, home_team TEXT, away_score TEXT, home_score TEXT, winner TEXT);")
 db.execute("CREATE TABLE IF NOT EXISTS batter_table(game_id TEXT, playerid TEXT, team TEXT, position TEXT, atbats TEXT, avg TEXT, baseonballs TEXT, doubles TEXT, "\
            "hits TEXT, homeruns TEXT, obp TEXT, ops TEXT, rbi TEXT, runs TEXT, slg TEXT, strikeouts TEXT, triples TEXT);")
-db.execute("CREATE TABLE IF NOT EXISTS pitcher_table(game_id TEXT, team TEXT, role TEXT, playerid TEXT, atbats TEXT, baseonballs TEXT, blownsaves TEXT, doubles TEXT, earnedruns TEXT, era TEXT, "\
-           "hits TEXT, holds TEXT, homeruns TEXT, inningspitched TEXT, losses TEXT, pitchesthrown TEXT, rbi TEXT, runs TEXT, strikeouts TEXT, strikes TEXT, triples TEXT, "\
-           "whip TEXT, wins TEXT);")
-db.execute("CREATE TABLE IF NOT EXISTS schedule();")
+# db.execute("CREATE TABLE IF NOT EXISTS pitcher_table(game_id TEXT, team TEXT, role TEXT, playerid TEXT, atbats TEXT, baseonballs TEXT, blownsaves TEXT, doubles TEXT, earnedruns TEXT, era TEXT, "\
+#            "hits TEXT, holds TEXT, homeruns TEXT, inningspitched TEXT, losses TEXT, pitchesthrown TEXT, rbi TEXT, runs TEXT, strikeouts TEXT, strikes TEXT, triples TEXT, "\
+#            "whip TEXT, wins TEXT);")
+# db.execute("CREATE TABLE IF NOT EXISTS schedule();")
           
 for i in data:
 #game_table insert query
-    game_table_sql = 'INSERT INTO game_table( game_id, game_date, away_team, home_team, away_score, home_score, winner) VALUES (' + \
-                     '\'' + str(i) + '\'' + ',' +  '\'' + data[i]['game_date'] + '\'' +  ',' + '\'' +  data[i]['away_team'] + '\'' +  ',' + \
-                     '\'' + data[i]['home_team'] + '\'' +  ',' + '\'' +  str(data[i]['away_score']) + '\'' +  ','  + '\'' + str(data[i]['home_score'])\
-                     + '\'' + ',' + '\'' +str(data[i]['winner']) + '\'' + ');'
-    db.execute(game_table_sql)
+    # game_table_sql = 'INSERT INTO game_table( game_id, game_date, away_team, home_team, away_score, home_score, winner) VALUES (' + \
+    #                  '\'' + str(i) + '\'' + ',' +  '\'' + data[i]['game_date'] + '\'' +  ',' + '\'' +  data[i]['away_team'] + '\'' +  ',' + \
+    #                  '\'' + data[i]['home_team'] + '\'' +  ',' + '\'' +  str(data[i]['away_score']) + '\'' +  ','  + '\'' + str(data[i]['home_score'])\
+    #                  + '\'' + ',' + '\'' +str(data[i]['winner']) + '\'' + ');'
+    # db.execute(game_table_sql)
     
 #pitcher_table insert query
-    for k in range(1, 5):
-        if k % 4 == 0:
-            key, team, role = 'home_bullpen_', 'home', 'bullpen'
-        elif k % 4 == 1:
-            key, team, role = 'away_starter_', 'away', 'starter'
-        elif k % 4 == 2:
-            key, team, role = 'away_bullpen_', 'away', 'bullpen'
-        elif k % 4 == 3:
-            key, team, role = 'home_starter_', 'home', 'starter'
+    # for k in range(1, 5):
+    #     if k % 4 == 0:
+    #         key, team, role = 'home_bullpen_', 'home', 'bullpen'
+    #     elif k % 4 == 1:
+    #         key, team, role = 'away_starter_', 'away', 'starter'
+    #     elif k % 4 == 2:
+    #         key, team, role = 'away_bullpen_', 'away', 'bullpen'
+    #     elif k % 4 == 3:
+    #         key, team, role = 'home_starter_', 'home', 'starter'
    
-        pitcher_table_sql = 'INSERT INTO pitcher_table( game_id, playerid, team, role, atbats, baseonballs, blownsaves, doubles, earnedruns, era,' \
-                      'hits, holds, homeruns, inningspitched, losses, pitchesthrown, rbi, runs, strikeouts, strikes, triples, whip, wins) VALUES (' + \
-                     '\'' + str(i) + '\'' + ',' +  '\'' + str(data[i][key + 'playerid']) + '\'' +  ',' + '\'' +  team + '\'' +  ',' + '\'' + role + '\'' +  ',' + \
-                     '\'' + str(data[i][key + 'atbats']) + '\'' +  ',' + '\'' + str(data[i][key + 'baseonballs']) + '\'' +  ',' + '\'' + str(data[i][key + 'blownsaves']) + '\'' +  ',' + \
-                     '\'' + str(data[i][key + 'doubles']) + '\'' +  ',' + '\'' + str(data[i][key + 'earnedruns']) + '\'' +  ',' + '\'' + str(data[i][key + 'era']) + '\'' + ',' + \
-                     '\'' + str(data[i][key + 'hits']) + '\'' +  ',' + '\'' + str(data[i][key + 'holds']) + '\'' +  ',' + '\'' + str(data[i][key + 'homeruns']) + '\'' +  ',' + \
-                     '\'' + str(data[i][key + 'inningspitched']) + '\'' +  ',' + '\'' + str(data[i][key + 'losses']) + '\'' +  ',' + '\'' + str(data[i][key + 'pitchesthrown']) + '\'' +  ',' + \
-                     '\'' + str(data[i][key + 'rbi']) + '\'' +  ',' + '\'' + str(data[i][key + 'runs']) + '\'' +  ',' + '\'' + str(data[i][key + 'strikeouts']) + '\'' +  ',' + \
-                     '\'' + str(data[i][key + 'strikes']) + '\'' +  ',' + '\'' + str(data[i][key + 'triples']) + '\'' +  ',' + '\'' + str(data[i][key + 'whip']) + '\'' +  ',' + \
-                     '\'' + str(data[i][key + 'wins']) + '\'' + ');'
+    #     pitcher_table_sql = 'INSERT INTO pitcher_table( game_id, playerid, team, role, atbats, baseonballs, blownsaves, doubles, earnedruns, era,' \
+    #                   'hits, holds, homeruns, inningspitched, losses, pitchesthrown, rbi, runs, strikeouts, strikes, triples, whip, wins) VALUES (' + \
+    #                  '\'' + str(i) + '\'' + ',' +  '\'' + str(data[i][key + 'playerid']) + '\'' +  ',' + '\'' +  team + '\'' +  ',' + '\'' + role + '\'' +  ',' + \
+    #                  '\'' + str(data[i][key + 'atbats']) + '\'' +  ',' + '\'' + str(data[i][key + 'baseonballs']) + '\'' +  ',' + '\'' + str(data[i][key + 'blownsaves']) + '\'' +  ',' + \
+    #                  '\'' + str(data[i][key + 'doubles']) + '\'' +  ',' + '\'' + str(data[i][key + 'earnedruns']) + '\'' +  ',' + '\'' + str(data[i][key + 'era']) + '\'' + ',' + \
+    #                  '\'' + str(data[i][key + 'hits']) + '\'' +  ',' + '\'' + str(data[i][key + 'holds']) + '\'' +  ',' + '\'' + str(data[i][key + 'homeruns']) + '\'' +  ',' + \
+    #                  '\'' + str(data[i][key + 'inningspitched']) + '\'' +  ',' + '\'' + str(data[i][key + 'losses']) + '\'' +  ',' + '\'' + str(data[i][key + 'pitchesthrown']) + '\'' +  ',' + \
+    #                  '\'' + str(data[i][key + 'rbi']) + '\'' +  ',' + '\'' + str(data[i][key + 'runs']) + '\'' +  ',' + '\'' + str(data[i][key + 'strikeouts']) + '\'' +  ',' + \
+    #                  '\'' + str(data[i][key + 'strikes']) + '\'' +  ',' + '\'' + str(data[i][key + 'triples']) + '\'' +  ',' + '\'' + str(data[i][key + 'whip']) + '\'' +  ',' + \
+    #                  '\'' + str(data[i][key + 'wins']) + '\'' + ');'
     
-        db.execute(pitcher_table_sql)
+    #     db.execute(pitcher_table_sql)
 
 #batter_table insert query
     for j in range(1, 19):

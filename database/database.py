1e928ff051838d04f8daf2189c799a69a6182a36
sp0t <Chenyin1g992@gmail.com>
2023-03-16 12:12:12 -0700

    [fix]-betting

--- database/database.py
+++ database/database.py
@@ -14,11 +14,11 @@ import uuid
 def connect_to_db(): 
     
     try: 
-        # engine = create_engine('postgresql://postgres:lucamlb123@ec2-3-115-115-146.ap-northeast-1.compute.amazonaws.com:5432/betmlb', connect_args = {'connect_timeout': 10}, echo=False, pool_size=20, max_overflow=0)
-        engine = create_engine('postgresql://postgres:123@localhost:5432/testdb', 
-                               connect_args = {'connect_timeout': 10}, 
-                               echo=False, pool_size=20, max_overflow=0)
-        print('Connection Initiated')
+        engine = create_engine('postgresql://postgres:lucamlb123@ec2-3-115-115-146.ap-northeast-1.compute.amazonaws.com:5432/betmlb', connect_args = {'connect_timeout': 10}, echo=False, pool_size=20, max_overflow=0)
+        # engine = create_engine('postgresql://postgres:123@localhost:5432/testdb', 
+        #                        connect_args = {'connect_timeout': 10}, 
+        #                        echo=False, pool_size=20, max_overflow=0)
+        # print('Connection Initiated')
     except:
         raise ValueError("Can't connect to Heroku PostgreSQL! You must be so embarrassed")
     return engine

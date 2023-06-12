dd9ef8a10582b47c9ec56ac419cbc79331904b7c
sp0t <chenyin1g992@gmail.com>
2023-06-12 17:19:49 -0600

    init

--- insertStats.py
+++ insertStats.py
@@ -31,7 +31,7 @@ for filename in dir_list:
         data.drop(data.iloc[:, 0:3], inplace=True, axis=1)
         for index, row in data.iterrows():
             engine.execute(text(f"INSERT INTO batter_stats(game_id, game_date, player_id, career_atBats, career_avg, career_homeRuns, career_obp, career_ops, career_rbi, career_slg, career_strikeOuts, recent_atBats, recent_avg, recent_homeRuns, recent_obp, recent_ops, recent_rbi, recent_slg, recent_strikeOuts) \
-                                VALUES('{gameid}', '{gamedate}', '{int(row['player_id'])}', '{row['career_atBats']}', '{row['career_avg']}', '{row['career_homeRuns']}', '{row['career_obp']}', '{row['career_ops']}', '{row['career_rbi']}', '{row['career_slg']}', '{row['career_strikeOuts']}', '{row['recent_atBats']}', '{row['recent_avg']}', '{row['recent_homeRuns']}', '{row['recent_obp']}', '{row['recent_ops']}', '{row['recent_rbi']}', '{row['recent_slg']}', '{row['recent_strikeOuts']}');"))
+                                VALUES('{gameid}', '{gamedate}', '{int(row['player_id'])}', '{round(row['career_atBats'], 3)}', '{round(row['career_avg'])}', '{round(row['career_homeRuns'])}', '{round(row['career_obp'])}', '{round(row['career_ops'])}', '{round(row['career_rbi'])}', '{round(row['career_slg'])}', '{round(row['career_strikeOuts'])}', '{round(row['recent_atBats'])}', '{round(row['recent_avg'])}', '{round(row['recent_homeRuns'])}', '{round(row['recent_obp'])}', '{round(row['recent_ops'])}', '{round(row['recent_rbi'])}', '{round(row['recent_slg'])}', '{round(row['recent_strikeOuts'])}');"))
 
     if role == 'PitcherData':
         data.drop(data.iloc[:, 0:3], inplace=True, axis=1)
@@ -40,4 +40,4 @@ for filename in dir_list:
             insertquery = f"INSERT INTO pitcher_stats(game_id, game_date, player_id, career_era, career_homeRuns, career_whip, career_battersFaced, recent_era, recent_homeRuns, recent_whip, recent_battersFaced)"
             print(insertquery)
             engine.execute(text(f"INSERT INTO pitcher_stats(game_id, game_date, player_id, career_era, career_homeRuns, career_whip, career_battersFaced, recent_era, recent_homeRuns, recent_whip, recent_battersFaced) \
-                                VALUES('{gameid}', '{gamedate}', '{int(row['player_id'])}', '{row['career_era']}', '{row['career_homeRuns']}', '{row['career_whip']}', '{row['career_battersFaced']}', '{row['recent_era']}', '{row['recent_homeRuns']}', '{row['recent_whip']}', '{row['recent_battersFaced']}');"))
+                                VALUES('{gameid}', '{gamedate}', '{int(row['player_id'])}', '{round(row['career_era'])}', '{round(row['career_homeRuns'])}', '{round(row['career_whip'])}', '{round(row['career_battersFaced'])}', '{round(row['recent_era'])}', '{round(row['recent_homeRuns'])}', '{round(row['recent_whip'])}', '{round(row['recent_battersFaced'])}');"))

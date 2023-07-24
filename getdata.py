9400dfdb1bfc938d8d353fcedd86062cc982308a
sp0t <chenyin1g992@gmail.com>
2023-07-24 07:48:09 +0100

    init

--- getdata.py
+++ getdata.py
@@ -12,6 +12,7 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.binary_location = "/path/to/google-chrome"
 driver = webdriver.Chrome('/home/.wdm/drivers/chromedriver',chrome_options=chrome_options)
 driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 

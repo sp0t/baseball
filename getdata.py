693706b9f20b32802cdff852904fbeb94da27591
sp0t <chenyin1g992@gmail.com>
2023-07-28 02:58:57 +0100

    init

--- getdata.py
+++ getdata.py
@@ -136,8 +136,8 @@ create_table(db_connection)
 #run webdriver
 options = Options()
 options.use_chromium = True
-# options.add_argument('--headless')
-# options.add_argument('--no-sandbox')
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
 options.add_argument(f'user-agent={user_agent}')

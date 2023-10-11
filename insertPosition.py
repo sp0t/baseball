13d0e4f2322d973ceb354bd58261121823d797bf
sp0t <chenyin1g992@gmail.com>
2023-10-11 14:29:48 -0300

    add: insert-position table

---
+++ insertPosition.py
@@ -0,0 +1,77 @@
+# Dependencies
+import statsapi as mlb
+import time
+from datetime import date, timedelta, datetime
+import numpy as np
+import pandas as pd
+import sqlite3
+from database import database
+from sqlalchemy import text
+import csv
+import requests
+from schedule import schedule
+import pickle
+import joblib
+
+engine = database.connect_to_db()
+engine.execute(text("CREATE TABLE IF NOT EXISTS position(game_id TEXT, game_date TEXT, team TEXT, role TEXT, C TEXT, B1 TEXT, B2 TEXT, B3 TEXT, SS TEXT, LF TEXT, CF TEXT, RF TEXT, DH TEXT);"))
+
+
+year = datetime.now().year
+res_game = pd.read_sql(text(f"SELECT * FROM game_table WHERE game_date LIKE '{year}%';"), con = engine).to_dict('records')
+
+for game in res_game:
+    print(game['game_id'], game['game_date'])
+    data = mlb.boxscore_data(game['game_id'])
+    awayBatters = data['awayBatters']
+    homeBatters = data['homeBatters']
+    gamedate = data['gameId'][0:10]
+    away = data['teamInfo']['away']['abbreviation']
+    home = data['teamInfo']['home']['abbreviation']
+
+    for batter in awayBatters:
+        if batter['personId'] != 0 and batter['substitution'] == False:
+            if batter['position'] == 'C':
+                away_c = batter['name'].replace("'", " ")
+            elif batter['position'] == '1B':
+                away_b1 = batter['name'].replace("'", " ")
+            elif batter['position'] == '2B':
+                away_b2 = batter['name'].replace("'", " ")
+            elif batter['position'] == '3B':
+                away_b3 = batter['name'].replace("'", " ")
+            elif batter['position'] == 'SS':
+                away_ss = batter['name'].replace("'", " ")
+            elif batter['position'] == 'LF':
+                away_lf = batter['name'].replace("'", " ")
+            elif batter['position'] == 'CF':
+                away_cf = batter['name'].replace("'", " ")
+            elif batter['position'] == 'RF':
+                away_rf = batter['name'].replace("'", " ")
+            elif batter['position'] == 'DH':
+                away_dh = batter['name'].replace("'", " ")
+
+    engine.execute(text(f"INSERT INTO position(game_id, game_date, team, role, c, b1, b2, b3, ss, lf, cf, rf, dh) \
+                                    VALUES('{game['game_id']}', '{gamedate}', '{away}', 'away', '{away_c}', '{away_b1}', '{away_b2}', '{away_b3}', '{away_ss}', '{away_lf}', '{away_cf}', '{away_rf}', '{away_dh}');"))
+    for batter in homeBatters:
+        if batter['personId'] != 0 and batter['substitution'] == False:
+            if batter['position'] == 'C':
+                home_c = batter['name'].replace("'", " ")
+            elif batter['position'] == '1B':
+                home_b1 = batter['name'].replace("'", " ")
+            elif batter['position'] == '2B':
+                home_b2 = batter['name'].replace("'", " ")
+            elif batter['position'] == '3B':
+                home_b3 = batter['name'].replace("'", " ")
+            elif batter['position'] == 'SS':
+                home_ss = batter['name'].replace("'", " ")
+            elif batter['position'] == 'LF':
+                home_lf = batter['name'].replace("'", " ")
+            elif batter['position'] == 'CF':
+                home_cf = batter['name'].replace("'", " ")
+            elif batter['position'] == 'RF':
+                home_rf = batter['name'].replace("'", " ")
+            elif batter['position'] == 'DH':
+                home_dh = batter['name'].replace("'", " ")
+
+    engine.execute(text(f"INSERT INTO position(game_id, game_date, team, role, c, b1, b2, b3, ss, lf, cf, rf, dh) \
+                                    VALUES('{game['game_id']}', '{gamedate}', '{home}', 'home', '{home_c}', '{home_b1}', '{home_b2}', '{home_b3}', '{home_ss}', '{home_lf}', '{home_cf}', '{home_rf}', '{home_dh}');"))
--- update.py
+++
@@ -1,4 +0,0 @@
-import statsapi as mlb
-import pandas as pd
-from datetime import date
-from database import database

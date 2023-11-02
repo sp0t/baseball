0ba081c5108829d0c8923f293cbd873f3d7b3c30
sp0t <Chenyin1g992@gmail.com>
2023-11-02 13:18:06 +0200

    [develop - auto_select_bot]

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ from schedule import schedule
 
 
 game_sched = mlb.schedule(start_date = "2023-11-01")
+for game in game_sched:
+    box = get_box_score(game['game_id'])
+    print(box['away_team'], box['home_team'])
 print(game_sched)
 # info_keys = ['game_id', 'game_datetime','away_name', 'home_name']
 # game_sched = [{k:v for k,v in el.items() if k in info_keys} for el in game_sched]

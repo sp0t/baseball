95009a897b296c34382266f1dc6deec622613fbe
sp0t <Chenyin1g992@gmail.com>
2023-11-07 10:42:55 +0200

    [test]-autoselect

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import statsapi as mlb
 from schedule import schedule
 
 
-game_sched = mlb.schedule(start_date = "2023-11-01")
+game_sched = mlb.schedule(start_date = "2023-10-01")
 for game in game_sched:
     data = mlb.boxscore_data(game['game_id'])
     game_date = data['gameId'][:10]

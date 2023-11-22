77e5f521a1094145dabb518d3962c210a1be3cc4
sp0t <Chenyin1g992@gmail.com>
2023-11-22 10:24:08 +0200

    [insert] - fill gameid of test_betting

--- test.py
+++ test.py
@@ -12,6 +12,8 @@ for bet in bet_res:
     gameid_res = pd.read_sql(text(f"SELECT * FROM game_table WHERE away_team = '{away_res[0]['team_abbr']}' AND home_team = '{home_res[0]['team_abbr']}' AND game_date = '{bet['betdate']}';"), con = engine).to_dict('records')
     if len(gameid_res) == 0:
         print(bet)
+        print(away_res)
+        print(gameid_res)
         continue
     print(gameid_res[0]['game_id'])
     # engine.execute(f"UPDATE test_betting SET game_id = '{gameid_res[0]['game_id']}' WHERE team1 = '{bet['team1']}' AND team2 = '{bet['team2']}' AND betdate = '{bet['betdate']}';") 

1f2bd22034bdd8d1fb4996d5cbf8c11aa8eac189
sp0t <Chenyin1g992@gmail.com>
2023-12-18 10:45:48 +0200

    [db]-create-odd-table

--- test.py
+++ test.py
@@ -1,17 +1,11 @@
-from database import database
+from sqlalchemy import create_engine
 import pandas as pd
-from sqlalchemy import text
+import statsapi as mlb
+from datetime import date, time, datetime, timedelta
+from pytz import timezone
+import numpy as np
+from database import database
 
 engine = database.connect_to_db()
 
-bet_res = pd.read_sql(text(f"SELECT * FROM test_betting;"), con = engine).to_dict('records')
-
-for bet in bet_res:
-    away_res = pd.read_sql(text(f"SELECT * FROM team_table WHERE team_name = '{bet['team1']}';"), con = engine).to_dict('records')
-    home_res = pd.read_sql(text(f"SELECT * FROM team_table WHERE team_name = '{bet['team2']}';"), con = engine).to_dict('records')
-    gameid_res = pd.read_sql(text(f"SELECT * FROM game_table WHERE away_team = '{away_res[0]['team_abbr']}' AND home_team = '{home_res[0]['team_abbr']}' AND game_date = '{bet['betdate']}';"), con = engine).to_dict('records')
-    if len(gameid_res) == 0:
-        gameid_res = pd.read_sql(text(f"SELECT * FROM game_table WHERE away_team = '{away_res[0]['team_abbr']}' AND home_team = '{home_res[0]['team_abbr']}' AND game_date = '{bet['betdate']}';"), con = engine).to_dict('records')
-        if len(gameid_res) == 0:
-            continue
-    engine.execute(f"UPDATE test_betting SET game_id = '{gameid_res[0]['game_id']}' WHERE team1 = '{bet['team1']}' AND team2 = '{bet['team2']}' AND betdate = '{bet['betdate']}';") 
+engine.execute("CREATE TABLE IF NOT EXISTS odds_table(game_id TEXT, game_date TEXT, away TEXT, home TEXT, start_time TEXT, away_open INT, away_close INT, home_open INT, home_close INT);")

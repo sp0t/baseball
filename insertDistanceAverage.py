6ebcde88269dd2a03eb06c014a259316f5081616
sp0t <Chenyin1g992@gmail.com>
2024-01-16 04:42:33 +0200

    [dev]-add distance average

--- insertDistanceAverage.py
+++ insertDistanceAverage.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 engine = database.connect_to_db()
 teams = ['OAK', 'PIT', 'SD', 'SEA', 'SF', 'STL', 'TB', 'TEX', 'TOR', 'MIN', 'PHI', 'ATL', 'CWS', 'MIA', 'NYY', 'MIL', 'LAA', 'AZ', 'BAL', 'BOS', 'CHC', 'CIN', 'CLE', 'COL', 'DET', 'HOU', 'KC', 'LAD', 'WSH', 'NYM']
-seasons = ['2021', '2023']
+seasons = ['2021', '2022', '2023']
 
 for team in teams:
     print('======================================================')

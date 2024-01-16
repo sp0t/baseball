b40669608ea7fd0ca167b6cee73e4396f37ffa17
sp0t <Chenyin1g992@gmail.com>
2024-01-16 04:38:34 +0200

    [dev]-add distance table

---
+++ insertDistanceAverage.py
@@ -0,0 +1,14 @@
+import itertools
+import math
+from sqlalchemy import text
+from database import database
+
+engine = database.connect_to_db()
+teams = ['OAK', 'PIT', 'SD', 'SEA', 'SF', 'STL', 'TB', 'TEX', 'TOR', 'MIN', 'PHI', 'ATL', 'CWS', 'MIA', 'NYY', 'MIL', 'LAA', 'AZ', 'BAL', 'BOS', 'CHC', 'CIN', 'CLE', 'COL', 'DET', 'HOU', 'KC', 'LAD', 'WSH', 'NYM']
+seasons = ['2021', '2023']
+
+for team in teams:
+    for season in seasons:
+        game_res = pd.read_sql(f"SELECT * FROM game_table WHERE (away_team = '{team}' OR home_team = '{team}') AND game_date LIKE '{season}%%';", con = engine).to_dict('records')
+        for game in game_res:
+            print(game)

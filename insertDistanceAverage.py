df4e2d64b23557e5927a35688cba5337e167eacc
sp0t <Chenyin1g992@gmail.com>
2024-01-17 15:01:57 +0200

    [add]-distacne-avaerage table

--- insertDistanceAverage.py
+++ insertDistanceAverage.py
@@ -19,7 +19,6 @@ for team in teams:
         pre_home_team = ''
         game_res = pd.read_sql(f"SELECT * FROM game_table WHERE (away_team = '{team}' OR home_team = '{team}') AND game_date LIKE '{season}%%';", con = engine).to_dict('records')
         for game in game_res:
-            print('current', game['away_team'], game['home_team'])
             if game['away_team'] == team:
                 count = count + 1
                 state = True
@@ -27,10 +26,10 @@ for team in teams:
                     print('===================1')
                     team1 = team
                     team2 = game['home_team']
-                elif pre_away_team == team:
-                    print('===================2')
-                    team1 = game['home_team']
-                    team2 = pre_home_team
+                elif pre_away_team == team and pre_home_team != game['home_team']:
+                    print('===================3')
+                    state = False
+                elif pre_away_team == team and pre_home_team == game['home_team']:
             elif game['home_team'] == team:
                 if pre_away_team == '' or pre_home_team == '' or pre_home_team == team:
                     print('===================3')
@@ -44,11 +43,9 @@ for team in teams:
                     team2 = pre_home_team
 
             if state == True:
-                print(team1, team2)
                 distance_res = pd.read_sql(f"SELECT * FROM distance_table WHERE (team1 = '{team1}' AND team2 = '{team2}') OR (team1 = '{team2}' AND team2 = '{team1}');", con = engine).to_dict('records')
                 
                 print(distance_res)
             pre_away_team = game['away_team']
             pre_home_team = game['home_team']
-            print('before', pre_away_team, pre_home_team)
             

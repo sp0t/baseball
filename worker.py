b7d4534584bd1400f6bc5b40e02d49315a1850a9
sp0t <Chenyin1g992@gmail.com>
2024-02-26 13:40:19 -0800

    [add]- add staking table

--- worker.py
+++ worker.py
@@ -1,18 +1,12 @@
-import sched
-import psycopg2
-from io import StringIO
-import statsapi as mlb
-from datetime import date, datetime, timedelta
-import time
+from sqlalchemy import create_engine
 import pandas as pd
-
+import statsapi as mlb
+from datetime import date, time, datetime, timedelta
+from pytz import timezone
+import numpy as np
 from database import database
-from schedule import schedule
-from passlib.hash import sha256_crypt
-#conn, cur = database.connect_to_db()
 
-# database.update_database()
-# schedule.update_schedule()
+engine = database.connect_to_db()
 
-
+engine.execute("CREATE TABLE IF NOT EXISTS staking_table(id SERIAL PRIMARY KEY, game_date TEXT, away TEXT, home TEXT, bet TEXT, american_odd INT, decimal_odd FLOAT(8), bet_size FLOAT(8), result TEXT, win_count INT, bet_count INT, bet_win FLOAT(8), risk_coeff FLOAT(8), stake_size FLOAT(8), pl_coeff FLOAT(8));")
 

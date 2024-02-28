1f9c98210dc37b7100f7509054ec3c1a99391597
sp0t <Chenyin1g992@gmail.com>
2024-02-28 11:17:46 -0800

    [develop] - add staking model

--- schedule/schedule.py
+++ schedule/schedule.py
@@ -112,6 +112,7 @@ def get_schedule_from_mlb():
     for el in game_sched: 
         el['game_datetime'] = el['game_datetime'].split('T')[1][:-1] 
         el['game_id'] = str(el['game_id'])
+        el['game_datetime'] = datetime.strptime(el['game_datetime'], '%H:%M:%S')
         el['game_datetime'] = el['game_datetime'].astimezone(tz) 
         el['game_datetime'] = datetime.strftime(el['game_datetime'], '%H:%M:%S')
         

6cd64a32cd26e40ecf5de3d33657d52bee24ca4b
sp0t <chenyin1g992@gmail.com.com>
2024-03-13 16:45:51 -0700

    [fix]-target value move to the homepage

--- worker.py
+++ worker.py
@@ -5,5 +5,8 @@ from datetime import date, time, datetime, timedelta
 from database import database
 from pytz import timezone
 
-data = mlb.boxscore_data("748104")
-print(data['awayPitchers'])
+# data = mlb.boxscore_data("748104")
+end_date = date.today()
+print(end_date)
+schedule = mlb.schedule(start_date=end_date)
+print(schedule)

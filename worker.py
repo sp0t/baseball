9a8fc7b44096ac3a562001cf74b4e0f711fc1e7d
sp0t <chenyin1g992@gmail.com.com>
2024-03-16 09:26:42 -0700

    [fix]-target value move to the homepage

--- worker.py
+++ worker.py
@@ -4,6 +4,7 @@ import pandas as pd
 from datetime import date, time, datetime, timedelta
 from database import database
 from pytz import timezone
+from zoneinfo import ZoneInfo
 
 
 game_sched = mlb.schedule(start_date = date.today())
@@ -12,11 +13,11 @@ game_sched = mlb.schedule(start_date = date.today())
 info_keys = ['game_id', 'game_datetime','away_name', 'home_name']
 game_sched = [{k:v for k,v in el.items() if k in info_keys} for el in game_sched]
     
-tz = timezone('US/Eastern')
+tz = ZoneInfo('America/New_York')
 for el in game_sched: 
     el['game_datetime'] = el['game_datetime'].split('T')[1][:-1] 
     el['game_id'] = str(el['game_id'])
     el['game_datetime'] = datetime.strptime(el['game_datetime'], '%H:%M:%S')
-    el['game_datetime'] = el['game_datetime'].astimezone(tz) 
+    el['game_datetime'] = el['game_datetime'].astimezone(tz)
     el['game_datetime'] = datetime.strftime(el['game_datetime'], '%H:%M:%S')
     print(el['game_datetime'])

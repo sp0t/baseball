99d8d6b2d8d219dfc6bb6434e66b4595b5a31ae0
sp0t <chenyin1g992@gmail.com.com>
2024-03-16 09:27:59 -0700

    [fix]-target value move to the homepage

--- worker.py
+++ worker.py
@@ -13,11 +13,11 @@ game_sched = mlb.schedule(start_date = date.today())
 info_keys = ['game_id', 'game_datetime','away_name', 'home_name']
 game_sched = [{k:v for k,v in el.items() if k in info_keys} for el in game_sched]
     
-tz = ZoneInfo('America/New_York')
+tz = timezone('America/New_York')
 for el in game_sched: 
     el['game_datetime'] = el['game_datetime'].split('T')[1][:-1] 
     el['game_id'] = str(el['game_id'])
     el['game_datetime'] = datetime.strptime(el['game_datetime'], '%H:%M:%S')
-    el['game_datetime'] = el['game_datetime'].astimezone(tz)
+    el['game_datetime'] = el['game_datetime'].astimezone(tz) 
     el['game_datetime'] = datetime.strftime(el['game_datetime'], '%H:%M:%S')
     print(el['game_datetime'])

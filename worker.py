00fb80a5ce650111afe0785a6e3c6347d07a06ff
sp0t <chenyin1g992@gmail.com.com>
2024-03-16 09:25:24 -0700

    [fix]-target value move to the homepage

--- worker.py
+++ worker.py
@@ -5,5 +5,18 @@ from datetime import date, time, datetime, timedelta
 from database import database
 from pytz import timezone
 
-data = mlb.boxscore_data("747989")
-print(data)
+
+game_sched = mlb.schedule(start_date = date.today())
+#testcommit
+# game_sched = mlb.schedule(start_date = "2023-11-01")
+info_keys = ['game_id', 'game_datetime','away_name', 'home_name']
+game_sched = [{k:v for k,v in el.items() if k in info_keys} for el in game_sched]
+    
+tz = timezone('US/Eastern')
+for el in game_sched: 
+    el['game_datetime'] = el['game_datetime'].split('T')[1][:-1] 
+    el['game_id'] = str(el['game_id'])
+    el['game_datetime'] = datetime.strptime(el['game_datetime'], '%H:%M:%S')
+    el['game_datetime'] = el['game_datetime'].astimezone(tz) 
+    el['game_datetime'] = datetime.strftime(el['game_datetime'], '%H:%M:%S')
+    print(el['game_datetime'])

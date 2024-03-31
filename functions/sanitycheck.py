a29205ab08fd764fe2aafee76a0f17cd9e3a46cb
sp0t <chenyin1g992@gmail.com.com>
2024-03-30 18:17:34 -0700

    [check]- position page

--- functions/sanitycheck.py
+++ functions/sanitycheck.py
@@ -142,6 +142,7 @@ def get_starter_df(player_id, year):
             f"a.strikes, a.triples, a.whip, a.wins FROM pitcher_table a LEFT JOIN game_table b ON a.game_id = b.game_id WHERE a.playerid = '{player_id}' ORDER BY game_date;"), con = engine)
 
     print(player_id)
+    print(df)
     
     string_cols = [col for col in df.columns if 'id' in col.lower()] + ['game_date', 'away_team', 'home_team']
 

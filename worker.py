66fd3f39b638a2027071d3cbc953e37a276a76e2
sp0t <chenyin1g992@gmail.com.com>
2024-04-01 05:09:46 -0700

    [check]- model c

--- worker.py
+++ worker.py
@@ -9,25 +9,177 @@ from schedule import schedule
 import requests
 
 
-boxscore = mlb.boxscore_data('745436')
-away_score = boxscore['awayBattingTotals']['r']
-home_score = boxscore['homeBattingTotals']['r']
-att_exists = False
-t_exists = False
+playerData = {}
+game_sched = mlb.schedule(start_date = date.today())
 
-for item in boxscore['gameBoxInfo']:
-    if item.get('label') == 'Att':
-        att_exists = True
-    elif item.get('label') == 'T':
-        t_exists = True
+for game in game_sched:
+    playerData[game['game_id']] = {}
+    playerData[game['game_id']]['away_batter'] = []
+    playerData[game['game_id']]['home_batter'] = []
+    playerData[game['game_id']]['away_pitcher'] = []
+    playerData[game['game_id']]['home_pitcher'] = []
+    playerData[game['game_id']]['name'] = {}
+    away_batter_atbats = {}
+    home_batter_atbats = {}
+    away_pitcher_atbats = {}
+    home_pitcher_atbats = {}
+    away_pitcher_played = {}
+    home_pitcher_played = {}
+    away_batter = []
+    home_batter = []
+    away_pitcher = []
+    home_pitcher = []
+    data = mlb.boxscore_data(game['game_id'])
+    game_date = data['gameId'][:10]
+    away_team = data['teamInfo']['away']['abbreviation']
+    home_team = data['teamInfo']['home']['abbreviation']
 
-data ={}
-data['away_score'] = 0
-data['home_score'] = 0
+    away_batter_res = pd.read_sql(text(f"SELECT * FROM (SELECT * FROM game_table WHERE away_team = '{data['teamInfo']['away']['abbreviation']}' or home_team = '{data['teamInfo']['away']['abbreviation']}' ORDER BY game_date DESC LIMIT 30) games INNER JOIN batter_table ON games.game_id = batter_table.game_id \
+        WHERE (games.away_team='{data['teamInfo']['away']['abbreviation']}' AND batter_table.team = 'away') OR (games.home_team='{data['teamInfo']['away']['abbreviation']}' AND batter_table.team = 'home');"), con = engine).to_dict('records')
 
-if att_exists == True and t_exists == True:
-    data['away_score'] = away_score
-    data['home_score'] = home_score  
+    home_batter_res = pd.read_sql(text(f"SELECT * FROM (SELECT * FROM game_table WHERE away_team = '{data['teamInfo']['home']['abbreviation']}' or home_team = '{data['teamInfo']['home']['abbreviation']}' ORDER BY game_date DESC LIMIT 30) games INNER JOIN batter_table ON games.game_id = batter_table.game_id \
+        WHERE (games.away_team='{data['teamInfo']['home']['abbreviation']}' AND batter_table.team = 'away') OR (games.home_team='{data['teamInfo']['home']['abbreviation']}' AND batter_table.team = 'home');"), con = engine).to_dict('records')
 
-print(data)
 
+    for away_player in away_batter_res:
+        if away_player['playerid'] in away_batter_atbats:
+            away_batter_atbats[away_player['playerid']] += int(away_player['atbats'])          
+        else:
+            away_batter_atbats[away_player['playerid']] = int(away_player['atbats'])
+
+    for home_player in home_batter_res:
+        if home_player['playerid'] in home_batter_atbats:
+            home_batter_atbats[home_player['playerid']] += int(home_player['atbats'])
+        else:
+            home_batter_atbats[home_player['playerid']] = int(home_player['atbats'])
+
+    away_batter_atbats_list = sorted(away_batter_atbats.items(), key=lambda x:x[1], reverse = True)
+    away_batter_sort_atbats = dict(away_batter_atbats_list)
+
+    home_batter_atbats_list = sorted(home_batter_atbats.items(), key=lambda x:x[1], reverse = True)
+    home_batter_sort_atbats = dict(home_batter_atbats_list)
+
+    away_pitcher_res = pd.read_sql(text(f"SELECT * FROM (SELECT * FROM game_table WHERE away_team = '{data['teamInfo']['away']['abbreviation']}' or home_team = '{data['teamInfo']['away']['abbreviation']}' ORDER BY game_date DESC LIMIT 30) games INNER JOIN pitcher_table ON games.game_id = pitcher_table.game_id \
+        WHERE (games.away_team='{data['teamInfo']['away']['abbreviation']}' AND pitcher_table.team = 'away' AND pitcher_table.role = 'bullpen') OR (games.home_team='{data['teamInfo']['away']['abbreviation']}' AND pitcher_table.team = 'home' AND pitcher_table.role = 'bullpen');"), con = engine).to_dict('records')
+
+    home_pitcher_res = pd.read_sql(text(f"SELECT * FROM (SELECT * FROM game_table WHERE away_team = '{data['teamInfo']['home']['abbreviation']}' or home_team = '{data['teamInfo']['home']['abbreviation']}' ORDER BY game_date DESC LIMIT 30) games INNER JOIN pitcher_table ON games.game_id = pitcher_table.game_id \
+        WHERE (games.away_team='{data['teamInfo']['home']['abbreviation']}' AND pitcher_table.team = 'away' AND pitcher_table.role = 'bullpen') OR (games.home_team='{data['teamInfo']['home']['abbreviation']}' AND pitcher_table.team = 'home' AND pitcher_table.role = 'bullpen');"), con = engine).to_dict('records')
+
+
+    for away_player in away_pitcher_res:
+        if away_player['playerid'] in away_pitcher_atbats:
+            away_pitcher_atbats[away_player['playerid']] += int(away_player['atbats'])
+        else:
+            away_pitcher_atbats[away_player['playerid']] = int(away_player['atbats'])
+
+        if away_player['playerid'] not in away_pitcher_played:
+            away_pitcher_played[away_player['playerid']] = {}
+            away_pitcher_played[away_player['playerid']]['gamedate'] = away_player['game_date']
+            away_pitcher_played[away_player['playerid']]['count'] = 1
+        else:
+            # print((datetime.combine(date.today(), datetime.min.time()) - datetime.strptime(away_player['game_date'], '%Y/%m/%d')).days)
+            if((datetime.strptime(away_player['game_date'], '%Y/%m/%d') - datetime.strptime(away_pitcher_played[away_player['playerid']]['gamedate'], '%Y/%m/%d')).days) == 1:
+                away_pitcher_played[away_player['playerid']]['count'] = 2
+            else:
+                away_pitcher_played[away_player['playerid']]['count'] = 1
+
+            away_pitcher_played[away_player['playerid']]['gamedate'] = away_player['game_date']
+
+    for home_player in home_pitcher_res:
+        if home_player['playerid'] in home_pitcher_atbats:
+            home_pitcher_atbats[home_player['playerid']] += int(home_player['atbats'])
+        else:
+            home_pitcher_atbats[home_player['playerid']] = int(home_player['atbats'])
+
+        if home_player['playerid'] not in home_pitcher_played:
+            home_pitcher_played[home_player['playerid']] = {}
+            home_pitcher_played[home_player['playerid']]['gamedate'] = home_player['game_date']
+            home_pitcher_played[home_player['playerid']]['count'] = 1
+        else:
+            if((datetime.strptime(home_player['game_date'], '%Y/%m/%d') - datetime.strptime(home_pitcher_played[home_player['playerid']]['gamedate'], '%Y/%m/%d')).days) == 1:
+                home_pitcher_played[home_player['playerid']]['count'] = 2
+            else:
+                home_pitcher_played[home_player['playerid']]['count'] = 1
+
+            home_pitcher_played[home_player['playerid']]['gamedate'] = home_player['game_date']
+
+    away_pitcher_atbats_list = sorted(away_pitcher_atbats.items(), key=lambda x:x[1], reverse = True)
+    away_pitcher_sort_atbats = dict(away_pitcher_atbats_list)
+
+
+    home_pitcher_atbats_list = sorted(home_pitcher_atbats.items(), key=lambda x:x[1], reverse = True)
+    home_pitcher_sort_atbats = dict(home_pitcher_atbats_list)
+
+
+    rosters = schedule.get_rosters(game['game_id'])
+    name = {}
+
+    for away in rosters['away']:
+        if 'fullName' in away:
+            name[str(away['id'])] = away['fullName']
+        else:
+            name[str(away['id'])] = ''
+
+    for home in rosters['home']:
+        if 'fullName' in home:
+            name[str(home['id'])] = home['fullName']
+        else:
+            name[str(home['id'])] = ''
+
+    count = 0
+    for key in away_batter_sort_atbats.keys():
+        if (count == 15):
+            break
+
+        if key not in name:
+            continue
+
+        away_batter.append(key)
+        count += 1
+    
+    count = 0
+    for key in home_batter_sort_atbats.keys():
+        if (count == 15):
+            break
+
+        if key not in name:
+            continue
+
+        home_batter.append(key)
+        count += 1
+    
+    count = 0
+    for key in away_pitcher_sort_atbats.keys():
+        if (count == 4):
+            break
+
+        if (datetime.combine(date.today(), datetime.min.time()) - datetime.strptime(away_pitcher_played[key]['gamedate'], '%Y/%m/%d')).days == 1 and away_pitcher_played[key]['count'] == 2:
+            continue
+
+        if key not in name:
+            continue
+
+        away_pitcher.append(key)
+        count += 1
+
+    count = 0
+    for key in home_pitcher_sort_atbats.keys():
+        if (count == 4):
+            break
+
+        if (datetime.combine(date.today(), datetime.min.time()) - datetime.strptime(home_pitcher_played[key]['gamedate'], '%Y/%m/%d')).days == 1 and home_pitcher_played[key]['count'] == 2:
+            continue
+
+        if key not in name:
+            continue
+
+        home_pitcher.append(key)
+        count += 1
+
+    playerData[game['game_id']]['away_batter'] = away_batter
+    playerData[game['game_id']]['home_batter'] = home_batter
+    playerData[game['game_id']]['away_pitcher'] = away_pitcher
+    playerData[game['game_id']]['home_pitcher'] = home_pitcher
+    playerData[game['game_id']]['name'] = name
+
+print(playerData)

4377cc8caff82b9f876debce0efc1532f779b7f1
sp0t <chenyin1g992@gmail.com.com>
2024-04-09 16:40:53 -0700

    [fix]- friend-team page problem

---
+++ insertOdd.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import statsapi as mlb
+from datetime import date, time, datetime, timedelta
+from pytz import timezone
+import numpy as np
+from database import database
+
+engine = database.connect_to_db()
+
+engine.execute("CREATE TABLE IF NOT EXISTS odds_table(game_id TEXT, game_date TEXT, away TEXT, home TEXT, start_time TEXT, away_open INT, away_close INT, home_open INT, home_close INT);")
+
+df = pd.read_csv("MLB_Basic.csv")
+data = df.set_index('game_id').to_dict('dict')

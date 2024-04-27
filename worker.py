d1cb6e5e8894987779b308b0446c54141e2f6a15
sp0t <chenyin1g992@gmail.com.com>
2024-04-27 11:41:03 -0700

    [develop] - backup database

--- worker.py
+++ worker.py
@@ -1,33 +1,3 @@
 import subprocess
 import datetime
 import os
-
-def backup_database():
-    # Current date to append to the backup file's name
-    date_str = datetime.datetime.now().strftime("%Y%m%d")
-    # Filename for the backup
-    filename = f"./backup/{date_str}.sql"
-    
-    # PostgreSQL credentials
-    db_username = "postgres"
-    db_host = "localhost"
-    db_port = "5432"
-    db_name = "betmlb"
-    db_password = "lucamlb123"  # Be cautious with password handling
-
-    # Setting the PGPASSWORD environment variable
-    os.environ['PGPASSWORD'] = db_password
-
-    # Command to run pg_dump
-    command = f"pg_dump -U {db_username} -h {db_host} -p {db_port} -d {db_name} -f {filename}"
-    
-    try:
-        # Execute the pg_dump command
-        subprocess.run(command, check=True, shell=True)
-        print("Backup successful")
-    finally:
-        # Ensure that the password is cleared from the environment variables after running
-        del os.environ['PGPASSWORD']
-
-# Run the backup function
-backup_database()

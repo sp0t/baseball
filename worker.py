aa59a69f08fdd8819f64d9802492b277e430b57b
sp0t <chenyin1g992@gmail.com.com>
2024-07-15 14:58:43 -0700

    [dev] - create win pro table

--- worker.py
+++ worker.py
@@ -12,7 +12,7 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(chrome_options=chrome_options)
+driver = webdriver.Chrome('/usr/bin/chromedriver',chrome_options=chrome_options)
 
 
 def connect_to_db(): 

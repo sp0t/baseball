426f0a25b246fb226fb6698189c2347c1c0877d4
sp0t <Chenyin1g992@gmail.com>
2024-07-26 18:57:40 -0700

    [dev]

--- worker.py
+++ worker.py
@@ -15,8 +15,8 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--remote-debugging-port=9222')
 chrome_options.add_argument("--disable-gpu")
-# driver = webdriver.Chrome('/usr/bin/chromedriver',chrome_options=chrome_options)
-driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+driver = webdriver.Chrome('/usr/bin/chromedriver',chrome_options=chrome_options)
+# driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 url = f"https://fightodds.io/recent-mma-events"
 print('###################################################')

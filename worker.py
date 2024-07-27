79a295820631f8a35c61f568b1d832c89f96dc61
sp0t <Chenyin1g992@gmail.com>
2024-07-26 19:18:36 -0700

    [dev]

--- worker.py
+++ worker.py
@@ -1,35 +1,11 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.options import Options
-from sqlalchemy import create_engine
-import pandas as pd
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--remote-debugging-port=9222')
-chrome_options.add_argument("--disable-gpu")
-driver = webdriver.Chrome('/usr/bin/chromedriver',chrome_options=chrome_options)
-# driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+# Automatically download and use the correct version of ChromeDriver
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
-url = f"https://fightodds.io/recent-mma-events"
-print('###################################################')
-print('##                                               ')
-print(f'##   {url}                                      ')
-print('##                                               ')
-print('###################################################')
-driver.get(url)
+# Open a website
+driver.get('http://www.google.com')
 
-wait = WebDriverWait(driver, 10)
-get_url = driver.current_url
-wait.until(EC.url_to_be(url))
-
-page_source = driver.page_source
-soup = BeautifulSoup(page_source)
-print(soup)
+# Close the browser
+driver.quit()

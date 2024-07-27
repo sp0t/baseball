d70918138273e9cf4bfde6d977d9171f4cf10d25
sp0t <Chenyin1g992@gmail.com>
2024-07-26 18:49:20 -0700

    [dev]

--- worker.py
+++ worker.py
@@ -13,6 +13,7 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--remote-debugging-port=9222')
+chrome_options.add_argument("--disable-gpu")
 # driver = webdriver.Chrome('/usr/bin/chromedriver',chrome_options=chrome_options)
 driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 

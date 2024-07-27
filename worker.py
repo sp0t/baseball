0f92125fe69158fe0c55df182a0e5edfd86b2da6
sp0t <Chenyin1g992@gmail.com>
2024-07-26 19:21:59 -0700

    [dev]

--- worker.py
+++ worker.py
@@ -1,11 +1,9 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+import chromedriver_autoinstaller
 
-# Automatically download and use the correct version of ChromeDriver
-driver = webdriver.Chrome(ChromeDriverManager().install())
+chromedriver_autoinstaller.install()
 
-# Open a website
-driver.get('http://www.google.com')
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
 
-# Close the browser
 driver.quit()

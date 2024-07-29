e11fc603218d262bbbfa1ff023b3bee3473ad782
sp0t <Chenyin1g992@gmail.com>
2024-07-28 17:07:38 -0700

    [dev]

--- worker.py
+++ worker.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
 options = Options()
@@ -12,22 +13,27 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-url = f"https://baseballsavant.mlb.com/gamefeed?date=6/21/2024&gamePk=744841&chartType=pitch&leg[%E2%80%A6]Filter=&resultFilter=&hf=winProbability&sportId=1&liveAb="
-print('###################################################')
-print('##                                               ')
-print(f'##   {url}                                      ')
-print('##                                               ')
-print('###################################################')
-driver.get(url)
+try:
+    url = f"https://baseballsavant.mlb.com/gamefeed?date=6/21/2024&gamePk=744841&chartType=pitch&leg[%E2%80%A6]Filter=&resultFilter=&hf=winProbability&sportId=1&liveAb="
+    print('###################################################')
+    print('##                                               ')
+    print(f'##   {url}                                      ')
+    print('##                                               ')
+    print('###################################################')
+    driver.get(url)
 
-wait = WebDriverWait(driver, 10)
-wait.until(EC.presence_of_element_located((By.ID, 'tableWinProbability_744841')))
-# get_url = driver.current_url
-# wait.until(EC.url_to_be(url))
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.ID, 'tableWinProbability_744841')))
+    # get_url = driver.current_url
+    # wait.until(EC.url_to_be(url))
 
-page_source = driver.page_source
-soup = BeautifulSoup(page_source, 'html.parser')
-table_data = soup.find('div', id='tableWinProbability_744841')
-print(table_data)
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
+    table_data = soup.find('div', id='tableWinProbability_744841')
+    print(table_data)
 
-driver.close()
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+finally:
+    driver.close()
